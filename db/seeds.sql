a5a337377cc54962e4c5f04f8629a15d746189a3
Michael Cota <mikecotahf@gmail.com>
2022-04-30 20:27:50 -0400

    updated seeds file for departments and roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,23 @@
+21 lines (18 sloc)  434 Bytes
+   
 INSERT INTO department (name)
 VALUES 
+("IT"),
+("Sales & Marketing"),
+("Finance & Accounting"),
+("Operations"),
 
 INSERT INTO role (title, salary, department_id)
 VALUES
+("Full Stack Developer", ),
+("Software Engineer", ),
+("Accountant", ), 
+("Financial Analyst", )
+("Marketing Coordinator", ), 
+("Sales Lead", ),
+("Project Manager", )
+("Operations Manager", )
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
