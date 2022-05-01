51c5d61bf87ed7bf699f0771f3c37dea4449e8fb
Michael Cota <mikecotahf@gmail.com>
2022-04-30 20:36:41 -0400

    updating server.js with inquirer prompts

--- server.js
+++ server.js
@@ -1,25 +1,151 @@
 // import mysql2
 const mysql = require('mysql2')
-
 // import inquirer 
 const inquirer = require('inquirer'); 
-
 // import console.table
 const cTable = require('console.table'); 
 
 // connection to database
-const connection = mysql.creatConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'test'
-}); 
+  password: 'Stripedsocks1!',
+  database: 'employee_db'
+});
 
-// port destination 
-const PORT = process.env.PORT || 3001; 
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  afterConnection();
+});
 
-// app expression
-const app = express(); 
+afterConnection = () => {
+  promptUser()
+};
 
-// express middleware 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json()); 
+// inquirer prompt for first action
+const promptUser = () => {
+  inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'choices', 
+      message: 'What would you like to do?',
+      choices: ['View all departments', 
+                'View all roles', 
+                'View all employees', 
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee',
+                'No Action']
+    }
+  ])
+  .then((answers) => {
+    const { choices } = answers; 
+    
+    if (choices === "View all departments") {
+      showDepartments()
+    }
+
+    if (choices === "View all roles") {
+      showRoles()
+    }
+
+    if (choices === "View all employees") {
+      showEmployees()
+    }
+
+    if (choices === "Add a department") {
+      addDepartment()
+    }
+    if (choices === "No Action") {
+      connection.end()
+    }
+  })
+};
+
+// function to show all departments 
+showDepartments = () => {
+  console.log('Showing all departments...\n');
+  const sql = `SELECT * FROM department`; 
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    connection.end() 
+  })
+};
+
+// function to show all roles 
+showRoles = () => {
+  console.log('Showing all roles...\n');
+  const sql = `SELECT role.*, department.name 
+               AS department
+               FROM role
+               LEFT JOIN department
+               ON role.department_id = department.id
+               WHERE role.id = ?`; 
+  
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err; 
+    console.table(rows); 
+    connection.end()
+  })
+};
+
+// function to show all employees 
+showEmployees = () => {
+  console.log('Showing all employees...\n'); 
+  const sql = `SELECT * FROM employee`; 
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err; 
+    console.table(rows);
+    connection.end()
+  })
+}
+
+// function to add a department 
+addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: 'input', 
+      name: 'addDept',
+      message: "What department do you want to add?"
+    }
+  ])
+  .then(answer => {
+    const sql = `INSERT INTO department (name)
+                 VALUES (?)`;
+    connection.query(sql, answer.addDept, (err, result) => {
+      if (err) throw err;
+      console.log('Added' + answer.addDept + " to departments!"); 
+
+      showDepartments();
+    })
+  })
+};
+
+// function to add a role 
+addRole = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'addRole',
+      message: "What is the employee's first name?"
+    }
+  ])
+  .then(answer => {
+
+  })
+}
+
+// function to add an employee
+addEmployee = () => {
+
+}
+
+// function to update an employee 
+updateEmployee = () => {
+
+}
